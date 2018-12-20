4606d46da1cdb99bca727180bbdf7a0d70408fc1
Yan-ren Tsai <elleryq@gmail.com>
2018-12-20 23:06:28 +0800

    Use python3
    * Upgrade modules

--- main.py
+++ main.py
@@ -12,14 +12,14 @@ from moneydj import get_local_funds, get_foreign_funds
 if __name__ == "__main__":
     if not os.path.exists("local_funds.json"):
         json.dump(
-            [fund.__dict__ for fund in get_local_funds()],
+            [fund._asdict() for fund in get_local_funds()],
             open('local_funds.json', 'wt'))
     else:
         print("already grabbed.")
 
     if not os.path.exists("foreign_funds.json"):
         json.dump(
-            [fund.__dict__ for fund in get_foreign_funds()],
+            [fund._asdict() for fund in get_foreign_funds()],
             open('foreign_funds.json', 'wt'))
     else:
         print("already grabbed.")
